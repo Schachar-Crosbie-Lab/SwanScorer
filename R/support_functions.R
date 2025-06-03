2395942cf105d92b4fd31c67392ed3fdfd8dd612
jclutton <jonathan.clutton@sickkids.ca>
2025-06-03 09:55:29 -0400

    Initial commit

--- R/support_functions.R
+++ R/support_functions.R
@@ -7,7 +7,7 @@
 }
 
 #### Global Variables ####
-#globalVariables(c("time","hr_bpm","datetime","time_bin","Name","bounds"))
+globalVariables(c("value"))
 
 #### clean_file function ####
 #' @name read_clean_file
@@ -25,8 +25,6 @@
 #'
 #' @returns A clean data frame ready for t-scores
 #'
-#' @section Notes for development:
-#'
 #'
 clean_file <- function(file_path = NULL) {
 
@@ -39,25 +37,25 @@ clean_file <- function(file_path = NULL) {
   df <- rio::import(file_path)
 
   # Check for Required Questions
-  required_cols <- c('swan1','swan2','swan3','swan4','swan5','swan6','swan7','swan8','swan9',
+  required_cols <- c('age','gender',
+                     'swan1','swan2','swan3','swan4','swan5','swan6','swan7','swan8','swan9',
                      'swan10','swan11','swan12','swan13','swan14','swan15','swan16','swan17','swan18')
 
   if(!all(required_cols %in% colnames(df))){
-    stop(paste('Please check the column names in your file. There should at least 18 columns, i.e. one for each question in the SWAN.',
+    stop(paste('Please check the column names in your file. There should at least 20 columns, i.e. one for each question in the SWAN.',
                'They are required to be named as follows:',
                paste(required_cols, collapse = ", ")))
   }
 
   # Check for impossible values
   df_long <- df |>
-    tidyr::pivot_longer(cols = all_of(required_cols)) |>
+    tidyr::pivot_longer(cols = dplyr::all_of(required_cols)) |>
     dplyr::filter(value > 3 | value < -3)
 
   if(nrow(df_long) > 0){
     stop(paste("There appear to be",nrow(df_long),"values above 3 or below -3 in the file. These are not possible in the SWAN test.",
                "Please correct or remove the rows from the filing before trying again."))
   }
-
 
   return(list(df = df))
 }
