8d134b91fd295a4f31ae1ac0eeb6df67100cf001
jclutton <jonathan.clutton@sickkids.ca>
2025-06-10 17:13:48 -0400

    Updated run_model language for better NA handling. Checked for reproducibility. Scores were reproduced but need deeper check before release

--- R/support_functions.R
+++ R/support_functions.R
@@ -11,7 +11,26 @@ globalVariables(c("value",'swan_tot_gender_tscores','sd',
                   'swan1','swan2','swan3','swan4','swan5','swan6','swan7','swan8','swan9',
                   'swan10','swan11','swan12','swan13','swan14','swan15','swan16','swan17','swan18',
                   'age','gender','p_respondent',
-                  'age18','female','youth','p_respondent'))
+                  'age18','female','youth','p_respondent',
+                  'type',
+                  "swan_gender_low", "swan_gender_pred", "swan_hi_gender_low",
+                  "swan_hi_gender_pred", "swan_hi_low", "swan_hi_pred", "swan_hi_pro",
+                  "swan_ia_gender_low", "swan_ia_gender_pred", "swan_ia_low", "swan_ia_pred",
+                  "swan_ia_pro", "swan_low", "swan_pred", "swan_pro"))
+
+#### Document Internal Data ####
+#' Expected Column Types
+#'
+#' A data dictionary for expected column types in the import file
+#'
+#' @format ## `col_type`
+#' A data frame with 24 rows and 2 columns:
+#' \describe{
+#'   \item{column}{Column name}
+#'   \item{type}{Data class}
+#'   ...
+#' }
+"col_type"
 
 #### clean_file function ####
 #' @name clean_file
@@ -27,6 +46,11 @@ globalVariables(c("value",'swan_tot_gender_tscores','sd',
 #' @importFrom rio get_ext
 #' @importFrom tidyr pivot_longer
 #' @importFrom dplyr filter
+#' @importFrom dplyr anti_join
+#' @importFrom dpyr across
+#' @importFrom dplyr all_of
+#' @importFrom dplyr rename
+#' @importFrom utils capture.output
 #'
 #' @returns A clean data frame ready for t-scores
 #'
@@ -54,6 +78,25 @@ clean_file <- function(file_path = NULL) {
     stop(paste('Please check the column names in your file. The file appears to be missing the following required columns...\n',
                paste(missing_cols, collapse = ", ")))
   }
+
+  # Check for data types
+
+  df_type <- df |>
+    dplyr::summarise(dplyr::across(dplyr::everything(), class)) |>
+    dplyr::select(required_dem_cols, required_test_cols) |>
+    tidyr::pivot_longer(cols = dplyr::all_of(c(required_dem_cols,required_test_cols)),
+                        names_to = 'column',
+                        values_to = 'type') |>
+    dplyr::anti_join(col_type, by = c('column','type')) |>
+    dplyr::rename(current_type = type) |>
+    dplyr::left_join(col_type, by = 'column') |>
+    dplyr::rename(expected_type = type)
+
+  if(nrow(df_type) > 0){
+    stop(paste("There appears to an issue with the format of your data. Please correct the columns listed below. Be sure to check for unexpected characters... \n",
+               paste0(utils::capture.output(print(df_type)), collapse = "\n")))
+  }
+
 
   # Check for impossible values
   df_long <- df |>
@@ -83,12 +126,6 @@ clean_file <- function(file_path = NULL) {
                "2 = Female \n"))
   }
 
-  # Check gender
-  if(!any(unique(df$p_respondent) %in% c('1','0', NA) )){
-    stop(paste("It appears as though some of your p_respondent values are formatted incorrectly. Parent respondent should be coded as... \n",
-               "1 = Parent Respondent \n",
-               "0 = Child/Youth Self-Report \n"))
-  }
 
   return(df = df)
 }
@@ -191,13 +228,20 @@ build_summary <- function(df = NULL) {
     dplyr::mutate(age18 = dplyr::case_when(age < 18 ~ age,
                                            age >= 18 ~ 18,
                                            T ~ age)) |>
+      # Update gender coding
+    dplyr::mutate(gender = as.character(gender)) |>
+    dplyr::mutate(gender = dplyr::case_when(gender %in% c("1","2") ~ gender,
+                                     T ~ NA_character_)) |>
+    dplyr::mutate(gender = as.numeric(gender)) |>
+    # Use same codings as Annie's script
     dplyr::mutate(female = dplyr::case_when(gender == 1 ~ 0,
                                             gender == 2 ~ 1)) |>
     dplyr::mutate(youth = dplyr::case_when(age < 12 ~ 0,
                                            age >= 12 ~ 1,
                                            T ~ NA)) |>
+
     # Reverse scores
-    dplyr::mutate(dplyr::across(c(ia_subdomain, hi_subdomain),
+    dplyr::mutate(dplyr::across(dplyr::all_of(c(ia_subdomain, hi_subdomain)),
                          ~-1*.x))
 
   #Inattentive
@@ -233,8 +277,169 @@ build_summary <- function(df = NULL) {
 #' @returns A data frame with t-scores
 #'
 #' @importFrom dplyr case_when
+#' @importFrom dplyr select
+#' @importFrom dplyr contains
 #'
 run_model <- function(df = NULL) {
+
+  #### Produce t-scores with gender
+  df_mod <- df |>
+    dplyr::mutate(swan_gender_pred = -4.0630359 - 0.3384133  * age18 + 1.7004264 * female + 1.5455007 *
+                    p_respondent - 8.3141252 * female * p_respondent) |>
+    dplyr::mutate(swan_gender_low = as.numeric((swan_pro - swan_gender_pred) < 0)) |>
+    dplyr::mutate(swan_gender_sd_pred = sqrt(325.95663 -
+                                               7.12465 * age18 +
+                                               13.4144 * female -
+                                               229.07860 * p_respondent +
+                                               106.69317 * swan_gender_low  +
+                                               29.39191 * age18 * p_respondent -
+                                               44.74060 * female * p_respondent)) |>
+    dplyr::mutate(res_adj = dplyr::case_when(female == 0 & youth == 0  ~ -0.06113885,
+                                             female == 0 & youth == 1  ~ -0.06736433,
+                                             female == 1  & youth == 0 ~ -0.07421668,
+                                             female ==1  & youth ==1 ~ -0.06374547)) |>
+    dplyr::mutate(sd_adj = dplyr::case_when(female == 0 & youth == 0  ~ 0.9986281,
+                                             female == 0 & youth == 1  ~ 0.9879085,
+                                             female == 1  & youth == 0 ~ 0.9976924,
+                                             female ==1  & youth ==1 ~ 0.9972819)) |>
+    dplyr::mutate(swan_tot_gender_tscores = dplyr::case_when(age18 < 12 & p_respondent == 0 ~ NA,
+                                                               T ~ (((swan_pro - swan_gender_pred) / swan_gender_sd_pred) + res_adj) / (sd_adj) * 10 + 50))
+
+ #### Full Test Across Gender
+  df_mod <- df_mod |>
+    dplyr::mutate(swan_pred = -3.3512518 - 0.3206639 * age18 - 2.5708190 * p_respondent) |>
+    dplyr::mutate(swan_low = as.numeric((swan_pro - swan_pred) < 0)) |>
+    dplyr::mutate(swan_sd_pred = sqrt(314.405841 -
+                                        4.962281 * age18 -
+                                        252.382387 * p_respondent +
+                                        156.755038 * swan_low +
+                                        30.436026 * age18 * p_respondent -
+                                        5.112260 * age18 * swan_low)) |>
+    dplyr::mutate(res_adj = dplyr::case_when(youth == 0  ~ -0.06810488,
+                                             youth == 1  ~ -0.05023800)) |>
+    dplyr::mutate(sd_adj = dplyr::case_when(youth == 0  ~ 0.9993120,
+                                            youth == 1  ~ 0.9889011)) |>
+    dplyr::mutate(swan_tot_tscores = dplyr::case_when(age18 < 12 & p_respondent == 0 ~ NA,
+                                                               T ~ (((swan_pro - swan_pred) / swan_sd_pred) + res_adj) / (sd_adj) * 10 + 50))
+
+  #### Inattentive Models with
+  df_mod <- df_mod |>
+    dplyr::mutate(swan_ia_gender_pred = 5.8020600 + 0.1024968 * age18 +
+                    3.4245032 * female + 1.9036940 * p_respondent -
+                    0.1688897 * age18 * female - 4.8939896 * female * p_respondent) |>
+    dplyr::mutate(swan_ia_gender_low = as.numeric((swan_ia_pro - swan_ia_gender_pred) < 0)) |>
+    dplyr::mutate(swan_ia_gender_sd_pred = sqrt(48.549114  +
+                                                  1.769995 * age18 +
+                                                  3.912498 * female -
+                                                  38.303116 *  p_respondent +
+                                                  45.919611 * swan_ia_gender_low +
+                                                  6.907424 *  age18 * p_respondent -
+                                                  2.897395 * age18 * swan_ia_gender_low -
+                                                  12.385480 * female * p_respondent))|>
+    dplyr::mutate(res_adj = dplyr::case_when(female == 0 & youth == 0  ~ -0.047669334,
+                                             female == 0 & youth == 1  ~ -0.001994747,
+                                             female == 1  & youth == 0 ~ -0.052532166,
+                                             female ==1  & youth ==1 ~ -0.011352696)) |>
+    dplyr::mutate(sd_adj = dplyr::case_when(female == 0 & youth == 0  ~ 0.9983462,
+                                            female == 0 & youth == 1  ~ 0.9892343,
+                                            female == 1  & youth == 0 ~ 1.0007317,
+                                            female ==1  & youth ==1 ~ 0.9974842)) |>
+    dplyr::mutate(swan_ia_gender_tscores = dplyr::case_when(age18 < 12 & p_respondent == 0 ~ NA,
+                                                               T ~ (((swan_ia_pro - swan_ia_gender_pred) / swan_ia_gender_sd_pred) + res_adj) / (sd_adj) * 10 + 50))
+
+
+
+
+  #### Inattentive across gender
+  df_mod <- df_mod |>
+    dplyr::mutate(swan_ia_pred = -3.7744681 - 0.6639846 * p_respondent) |>
+    dplyr::mutate(swan_ia_low = as.numeric((swan_ia_pro - swan_ia_pred) < 0)) |>
+    dplyr::mutate(swan_ia_sd_pred = sqrt(43.782402 +
+                                        2.444014 * age18 -
+                                        43.067165 * p_respondent +
+                                        57.574635 * swan_ia_low +
+                                        7.026785 * age18 * p_respondent -
+                                        4.044906 * age18 * swan_ia_low)) |>
+    dplyr::mutate(res_adj = dplyr::case_when(youth == 0  ~ -0.050939400,
+                                             youth == 1  ~ -0.001064299)) |>
+    dplyr::mutate(sd_adj = dplyr::case_when(youth == 0  ~ 0.9993775,
+                                            youth == 1  ~ 0.9932011)) |>
+    dplyr::mutate(swan_ia_tscores = dplyr::case_when(age18 < 12 & p_respondent == 0 ~ NA,
+                                                        T ~ (((swan_ia_pro - swan_ia_pred) / swan_ia_sd_pred) + res_adj) / (sd_adj) * 10 + 50))
+
+
+  #### Hyperactive with gender
+  df_mod <- df_mod |>
+    dplyr::mutate(swan_hi_gender_pred = -4.17675209 - 0.04167303 * age18 + 0.70229528 * female + 5.13833773 *
+                                        p_respondent - 0.35179042 * age18 * p_respondent - 4.35765313 * female * p_respondent) |>
+    dplyr::mutate(swan_hi_gender_low = as.numeric((swan_hi_pro - swan_hi_gender_pred) < 0)) |>
+    dplyr::mutate(swan_hi_gender_sd_pred = sqrt(97.2118786  -
+                                                  1.6070110 * age18 -
+                                                  17.5869608 * female -
+                                                  62.4036863 * p_respondent +
+                                                  7.5639752 * swan_hi_gender_low  +
+                                                  0.5922562 * age18 * female +
+                                                  6.4226127 * age18 * p_respondent +
+                                                  1.3285283 * age18 * swan_hi_gender_low +
+                                                  4.5001409  * female * p_respondent +
+                                                  78.7787513 * female * swan_hi_gender_low +
+                                                  19.6268534 * p_respondent * swan_hi_gender_low -
+                                                  3.7360877 * age18 * female *  swan_hi_gender_low -
+                                                  49.7954285 * female * p_respondent * swan_hi_gender_low))|>
+    dplyr::mutate(res_adj = dplyr::case_when(female == 0 & youth == 0  ~ -0.07854678,
+                                             female == 0 & youth == 1  ~ -0.07142296,
+                                             female == 1  & youth == 0 ~ -0.08761418,
+                                             female ==1  & youth ==1 ~ -0.09748210)) |>
+    dplyr::mutate(sd_adj = dplyr::case_when(female == 0 & youth == 0  ~ 1.0002075,
+                                            female == 0 & youth == 1  ~ 0.9772225,
+                                            female == 1  & youth == 0 ~ 0.9956266,
+                                            female ==1  & youth ==1 ~ 0.9976900)) |>
+    dplyr::mutate(swan_hi_gender_tscores = dplyr::case_when(age18 < 12 & p_respondent == 0 ~ NA,
+                                                               T ~ (((swan_hi_pro - swan_hi_gender_pred) / swan_hi_gender_sd_pred) + res_adj) / (sd_adj) * 10 + 50))
+
+
+  #### Hyperactive across Gender
+  df_mod <- df_mod |>
+    dplyr::mutate(swan_hi_pred = -3.85277159 -
+                    0.03568567 * age18 + 2.93347689 * p_respondent -
+                    0.34794969 * age18 *
+                    p_respondent) |>
+    dplyr::mutate(swan_hi_low = as.numeric((swan_hi_pro - swan_hi_pred) < 0)) |>
+    dplyr::mutate(swan_hi_sd_pred = sqrt(95.490458 -
+                                           1.760477 * age18 -
+                                           63.468222 * p_respondent +
+                                           38.526578 * swan_hi_low +
+                                           6.763566 * age18 * p_respondent)) |>
+    dplyr::mutate(res_adj = dplyr::case_when(youth == 0  ~ -0.08224601,
+                                             youth == 1  ~ -0.08580553)) |>
+    dplyr::mutate(sd_adj = dplyr::case_when(youth == 0  ~ 0.9980346,
+                                            youth == 1  ~ 0.9868408)) |>
+    dplyr::mutate(swan_hi_tscores = dplyr::case_when(age18 < 12 & p_respondent == 0 ~ NA,
+                                                           T ~ (((swan_hi_pro - swan_hi_pred) / swan_hi_sd_pred) + res_adj) / (sd_adj) * 10 + 50))
+
+  #### Remove extra columns ####
+  df_final <- df_mod |>
+    dplyr::select(-dplyr::contains("pred"), -dplyr::contains("low"), -dplyr::contains("adj"))
+
+
+  return(df = df_final)
+}
+
+#' @name run_model_old
+#'
+#' @title An old version of the model, initially written in Annie's language
+#'
+#' @description This is the generic model used to create t-scores. It adjusts for gender, age, respondent, and time.
+#' It is best used in all cases unless a team is trying to look at values over time
+#' Use the dataframe from [build_summary()] to produce t-scores
+#'
+#' @param df should be a data.frame from [build_summary()]
+#'
+#' @returns A data frame with t-scores
+#'
+#' @importFrom dplyr case_when
+#'
+run_model_old <- function(df = NULL) {
 
   #### Produce t-scores with gender
   swan_gender_pred <-
@@ -274,17 +479,17 @@ run_model <- function(df = NULL) {
         as.logical(df$youth) ~ 0.9972819
     )
 
-  df$swan_gender_study_tscores <- (((df$swan_pro - swan_gender_pred) / swan_gender_sd_pred
+  df$swan_tot_gender_tscores <- (((df$swan_pro - swan_gender_pred) / swan_gender_sd_pred
   ) + res_adj) / (sd_adj) * 10 + 50
 
-  df$swan_gender_study_tscores <-
+  df$swan_tot_gender_tscores <-
     ifelse((df$age18 < 12) &
              (df$p_respondent == 0),
            NA,
            df$swan_tot_gender_tscores
     )
 
- #### Full Test Across Gender
+  #### Full Test Across Gender
 
   swan_pred <- -3.3512518 - 0.3206639 * df$age18 - 2.5708190 * df$p_respondent
 
@@ -307,9 +512,9 @@ run_model <- function(df = NULL) {
       as.logical(df$youth)  ~ 0.9889011
     )
 
-  df$swan_study_tscores <- (((df$swan_pro - swan_pred) / swan_sd_pred) + res_adj) / (sd_adj) * 10 + 50
+  df$swan_tot_tscores <- (((df$swan_pro - swan_pred) / swan_sd_pred) + res_adj) / (sd_adj) * 10 + 50
 
-  df$swan_study_tscores <-
+  df$swan_tot_tscores <-
     ifelse((df$age18 < 12) &
              (df$p_respondent == 0),
            NA,
@@ -354,10 +559,10 @@ run_model <- function(df = NULL) {
         as.logical(df$youth) ~ 0.9974842
     )
 
-  df$swan_ia_gender_study_tscores <- (((df$swan_ia_pro - swan_ia_gender_pred) / swan_ia_gender_sd_pred
+  df$swan_ia_gender_tscores <- (((df$swan_ia_pro - swan_ia_gender_pred) / swan_ia_gender_sd_pred
   ) + res_adj) / (sd_adj) * 10 + 50
 
-  df$swan_ia_gender_study_tscores <-
+  df$swan_ia_gender_tscores <-
     ifelse((df$age18 < 12) &
              (df$p_respondent == 0),
            NA,
@@ -386,10 +591,10 @@ run_model <- function(df = NULL) {
       as.logical(df$youth)  ~ 0.9932011
     )
 
-  df$swan_ia_study_tscores <- (((df$swan_ia_pro - swan_ia_pred) / swan_ia_sd_pred
+  df$swan_ia_tscores <- (((df$swan_ia_pro - swan_ia_pred) / swan_ia_sd_pred
   ) + res_adj) / (sd_adj) * 10 + 50
 
-  df$swan_ia_study_tscores <-
+  df$swan_ia_tscores <-
     ifelse((df$age18 < 12) &
              (df$p_respondent == 0),
            NA,
@@ -438,10 +643,10 @@ run_model <- function(df = NULL) {
         as.logical(df$youth) ~ 0.9976900
     )
 
-  df$swan_hi_gender_study_tscores <- (((df$swan_hi_pro - swan_hi_gender_pred) / swan_hi_gender_sd_pred
+  df$swan_hi_gender_tscores <- (((df$swan_hi_pro - swan_hi_gender_pred) / swan_hi_gender_sd_pred
   ) + res_adj) / (sd_adj) * 10 + 50
 
-  df$swan_hi_gender_study_tscores <-
+  df$swan_hi_gender_tscores <-
     ifelse((df$age18 < 12) &
              (df$p_respondent == 0),
            NA,
@@ -471,10 +676,10 @@ run_model <- function(df = NULL) {
       as.logical(df$youth)  ~ 0.9868408
     )
 
-  df$swan_hi_study_tscores <- (((df$swan_hi_pro - swan_hi_pred) / swan_hi_sd_pred
+  df$swan_hi_tscores <- (((df$swan_hi_pro - swan_hi_pred) / swan_hi_sd_pred
   ) + res_adj) / (sd_adj) * 10 + 50
 
-  df$swan_hi_study_tscores <-
+  df$swan_hi_tscores <-
     ifelse((df$age18 < 12) &
              (df$p_respondent == 0),
            NA,
@@ -483,4 +688,3 @@ run_model <- function(df = NULL) {
 
   return(df = df)
 }
-
